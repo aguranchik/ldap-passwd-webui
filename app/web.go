3272692ddda5efc55a253866e9e34da4a36184e4
guranchik <93979700+aguranchik@users.noreply.github.com>
2023-05-17 15:40:19 +0300

    Update web.go

--- app/web.go
+++ app/web.go
@@ -117,8 +117,7 @@ func ChangePassword(w http.ResponseWriter, req *http.Request) {
 	if len(alerts) == 0 {
 		client := NewLDAPServer()
 		if err := ModifyPassword(client, un, oldPassword[0], newPassword[0]); err != nil {
-//			alerts["error"] = fmt.Sprintf("%v", err)
-			alerts["error"] = "Password does not match requirements"
+			alerts["error"] = fmt.Sprintf("%v", err)
 		} else {
 			alerts["success"] = "Password successfuly changed"
 		}
